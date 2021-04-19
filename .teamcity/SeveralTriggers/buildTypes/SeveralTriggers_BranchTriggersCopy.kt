72bf60083ac3e0dbd23b0aaec38afdbfc6c645b4
admin <admin@TeamCity>
2021-04-19 17:59:12 +0300

    TeamCity change in 'Several Triggers' project: triggers of 'Branch triggers_copy' build configuration were updated

--- .teamcity/SeveralTriggers/buildTypes/SeveralTriggers_BranchTriggersCopy.kt
+++ .teamcity/SeveralTriggers/buildTypes/SeveralTriggers_BranchTriggersCopy.kt
@@ -1,9 +1,19 @@
 package SeveralTriggers.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 object SeveralTriggers_BranchTriggersCopy : BuildType({
     templates(SeveralTriggers_Template)
     uuid = "edf9969b-b773-4395-8349-664763097d7d"
     name = "Branch triggers_copy"
+
+    triggers {
+        vcs {
+            id = "TRIGGER_1"
+            quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_CUSTOM
+            quietPeriod = 69
+        }
+    }
 })
