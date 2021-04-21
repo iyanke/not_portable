7927561760e3f8859a5c4de87fc98d9d88e17247
admin <admin@TeamCity>
2021-04-21 11:08:00 +0300

    TeamCity change in 'Several Triggers' project: general settings of 'Trigger per VCS root' build configuration were updated

--- .teamcity/SeveralTriggers/buildTypes/SeveralTriggers_Build.kt
+++ .teamcity/SeveralTriggers/buildTypes/SeveralTriggers_Build.kt
@@ -8,7 +8,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 object SeveralTriggers_Build : BuildType({
     uuid = "bd0d8e9a-5302-482e-8a26-3318054d5a33"
-    name = "Build"
+    name = "Trigger per VCS root"
 
     vcs {
         root(SeveralTriggers.vcsRoots.SeveralTriggers_HttpsGithubComIyankeThird1refsHeadsMain)
